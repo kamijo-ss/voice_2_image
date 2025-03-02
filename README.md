7f12946bf3f8bfb273fa9437e6ae6304bd8c97c4
kamijo <ykamijo@sousou-official.com>
2025-03-02 23:29:19 +0900

    ini commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## 前置き
+https://github.com/bes-dev/stable_diffusion.openvino にUIと音声入力をつけたものです  
+音声の翻訳に https://huggingface.co/staka/fugumt-ja-en を使わせて頂いてます  
+プロジェクト管理はuvでやります  
+GPUを使うのでIntel Core Ultraシリーズ搭載のPCを想定しています
+
+
+## このレポジトリをクローンしたらやること
+1. **uvをインストール**  
+powershell -ExecutionPolicy Bypass -Command "irm https://astral.sh/uv/install.ps1 | iex"
+
+2. **モデル配置**  
+https://huggingface.co/bes-dev/stable-diffusion-v1-4-openvino/blob/main/unet.bin  
+https://huggingface.co/bes-dev/stable-diffusion-v1-4-openvino/blob/main/text_encoder.bin  
+をダウンロードし、名前を変えずにassets/stable-diffusion-v1-4-openvinoに保存してください  
+https://huggingface.co/staka/fugumt-ja-en/blob/main/pytorch_model.bin  
+をダウンロードし、名前を変えずにassets/fugumt-ja-enに保存してください  
+https://alphacephei.com/vosk/models/vosk-model-ja-0.22.zip  
+からVoskを落として解凍し、「vosk-model-ja-0.22」って名前でassets直下に保存してください  
+（別途モデルのDL用スクリプト組んでもよかったが億劫だった）
+
+3. **仮想環境作成**  
+cd stable_diffusion_uv  
+uv sync  
+uv run stable_diffusionって叩いて1分くらい待つとアプリが起動します
+
+**（おまけ1）UI編集のについて**  
+stable_diffusion_uv\\.venv\Lib\site-packages\qt5_applications\Qt\bin\designer を使ってfront.uiを編集  
+pyuic5 -x front.ui -o front.py　でfront.uiからfront.pyが出来上がります 
+
+
+**（おまけ2）ビルド**  
+ビルドもできるようになっています  
+uv build  
+uv tool install dist\stable_diffusion-0.1.0-py3-none-any.whl  
+（.exeファイルから起動する時はassets以下も同一階層に置いてください）
+
+
+## 音声が認識されない時は
+uv run device_search.py  
+↑音声デバイスの一覧が表示されます  
+お使いの入力デバイスのIDを控えてsrc/stable_diffusion/main.pyの26行目(device_id)に記述してください
---
+++ src/stable_diffusion/main.py
@@ -0,0 +1,181 @@
+import sys
+import argparse
+import random
+import json
+
+import numpy as np
+import cv2
+import pyaudio
+import torch
+from vosk import Model, KaldiRecognizer
+from transformers import MarianTokenizer, MarianMTModel
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtCore import QThread, pyqtSignal
+from diffusers import LMSDiscreteScheduler
+
+from stable_diffusion.front import Ui_Form
+from stable_diffusion.stable_diffusion_engine import StableDiffusionEngine
+
+class SpeechToTextThread(QThread):
+    # stringを投げるシグナルを定義
+    text_received = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.vosk = parent.vosk
+        self.device_id  = 1                 # pyaudioのget_device_info_by_indexで入力デバイスのIDを調べて設定
+        self.format     = pyaudio.paInt16   # 16bit
+        self.channels   = 1                 # Voskがモノラルに最適化されてるのでモノラルにする
+        self.rate       = 16000             # サンプルレートもVoskに合わせる
+        self.chunk      = 4096              # データのチャンクサイズ
+        self.running    = False
+    
+    def run(self):
+        voice = self.recording_voice()
+        # シグナルはemitメソッドを叩くと送信される
+        self.text_received.emit(voice)
+
+    def recording_voice(self):
+        audio = pyaudio.PyAudio()
+        stream = audio.open(
+            input_device_index=self.device_id,
+            format=self.format,
+            channels=self.channels,
+            frames_per_buffer=self.chunk,
+            rate=self.rate,
+            input=True
+        )
+        stream.start_stream()
+        rec = KaldiRecognizer(self.vosk, self.rate)
+        recording = True
+        while recording:
+            data = stream.read(self.chunk // 2, exception_on_overflow=False)
+            if rec.AcceptWaveform(data):
+                recording = False
+                stream.close()
+                audio.terminate()
+                self.quit()
+                return json.loads(rec.Result()).get("text")
+
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.sd_model = None
+        self.scheduler = None
+        try:
+            self.load_sd()
+            self.vosk = Model("assets/vosk-model-ja-0.22")
+            self.device = torch.device("cpu")
+            self.translate_model = MarianMTModel.from_pretrained("assets/fugumt-ja-en")
+            self.translate_model.eval()
+            self.translate_model = self.translate_model.to(self.device)
+            self.tokenizer = MarianTokenizer.from_pretrained("assets/fugumt-ja-en")
+            # スレッドのtext_receivedシグナルにgenerate_imageメソッドを接続
+            self.transcription_thread = SpeechToTextThread(self)
+            self.transcription_thread.text_received.connect(self.generate_image)
+        except Exception as e:
+            QMessageBox.critical(self, "エラー", f"モデルの読み込みに失敗しました: {str(e)}")
+            sys.exit(1)
+
+        self.ui.pushButton.clicked.connect(self.start_recording_thread)
+
+    def load_sd(self):
+        args = argparse.Namespace(
+            model="assets/stable-diffusion-v1-4-openvino",
+            device="GPU", # GPU環境がない人はここにCPUを設定
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            tokenizer="assets/clip-vit-large-patch14"
+        )
+
+        self.scheduler = LMSDiscreteScheduler(
+            beta_start=args.beta_start,
+            beta_end=args.beta_end,
+            beta_schedule=args.beta_schedule,
+            tensor_format="np"
+        )
+
+        self.sd_model = StableDiffusionEngine(
+            model=args.model,
+            scheduler=self.scheduler,
+            tokenizer=args.tokenizer,
+            device=args.device
+        )
+
+    def start_recording_thread(self):
+        self.ui.label.setText("recording...")
+        self.ui.label_2.setText("録音中...")
+        self.ui.textBrowser.setText("")
+        self.ui.textBrowser_2.setText("")
+        QApplication.processEvents()
+        self.ui.pushButton.setEnabled(False)
+        # startした時にスレッドのrunメソッドが走る
+        self.transcription_thread.start()
+
+    def translate_voice(self, voice):
+        inputs = self.tokenizer(voice, return_tensors="pt", padding=True, truncation=True)
+        inputs = {key: value.to(self.device) for key, value in inputs.items()}
+        with torch.no_grad():
+            return self.translate_model.generate(**inputs)
+
+    def generate_image(self,voice):
+        prompt = self.tokenizer.decode(self.translate_voice(voice)[0], skip_special_tokens=True)
+        self.ui.label_2.setText("generating...")
+        self.ui.label_2.setText("検出しました 生成中...")
+        self.ui.textBrowser.setText(prompt)
+        self.ui.textBrowser_2.setText(voice)
+        QApplication.processEvents()
+        seed = None
+        
+        try:
+            args = argparse.Namespace(
+                seed=seed,
+                num_inference_steps=8, # 8～32くらいの間でお好みで調整
+                guidance_scale=7.5,
+                eta=0.0,
+                prompt=prompt,
+                init_image=None,
+                strength=0.5,
+                mask=None
+            )
+            image = self.run_image_generation(args)
+            self.show_image(image)
+
+        except Exception as e:
+            QMessageBox.critical(self, "エラー", str(e))
+
+    def run_image_generation(self, args):
+        if args.seed is None:
+            args.seed = random.randint(0, 2**30)
+        np.random.seed(args.seed)
+
+        image = self.sd_model(
+            prompt=args.prompt,
+            init_image=None,
+            mask=None,
+            strength=args.strength,
+            num_inference_steps=args.num_inference_steps,
+            guidance_scale=args.guidance_scale,
+            eta=args.eta
+        )
+        self.ui.label.setText("generated")
+        self.ui.label_2.setText("生成完了")
+        self.ui.pushButton.setEnabled(True)
+
+        return image
+
+    def show_image(self, image):
+        cv2.imshow("Generated Image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+def main():
+    app = QApplication([])
+    window = MyApp()
+    window.show()
+    app.exec_()
